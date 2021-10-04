f19a22f6949ab33f2d0a925a4462010517f71dff
tannerdsilva <t@tannersilva.com>
2021-10-04 22:20:20 +0000

    built out more robust protocols

--- Sources/AddressKit/Protocol.swift
+++ Sources/AddressKit/Protocol.swift
@@ -3,4 +3,18 @@ public protocol Address:Comparable, Hashable {
 
 	var isPrivate:Bool { get }
 	var isReserved:Bool { get }
+	
+	init?(_:String)
+}
+
+public protocol Range:Hashable {
+	var string:String { get }
+	
+	init?(_:String)
+}
+
+public protocol Network:Hashable {
+	var cidrString:String { get }
+	
+	init?(cidr:String)
 }
